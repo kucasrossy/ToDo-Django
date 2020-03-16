a23de1cff80e4b0b2d607cbb05f830efc36a74e3
kucasrossy <lucasrossy270@gmail.com>
2020-03-16 16:02:17 -0300

    V0.7

--- tasks/views.py
+++ tasks/views.py
@@ -3,17 +3,19 @@ from django.core.paginator import Paginator
 from .models import Task
 from .forms import TaskForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
 def taskList(request):
     tasks_list = Task.objects.all().order_by('-created_at')
 
     search = request.GET.get('search')
 
     if search:
-        tasks = Task.objects.filter(title__icontains=search)
+        tasks = Task.objects.filter(title__icontains=search, user=request.user)
     else:
-        tasks_list = Task.objects.all().order_by('-created_at')
+        tasks_list = Task.objects.all().order_by('-created_at').filter(user=request.user)
 
         paginator = Paginator(tasks_list, 3)
 
@@ -22,10 +24,12 @@ def taskList(request):
         
     return render(request, 'tasks/list.html', {'tasks':tasks})
 
+@login_required
 def taskView(request, id):
     task = get_object_or_404(Task, pk = id)
     return render(request,'tasks/task.html', {'task' : task})
 
+@login_required
 def newTask(request):
      if request.method == 'POST':
             form = TaskForm(request.POST)
@@ -33,12 +37,14 @@ def newTask(request):
             if form.is_valid():
                 task = form.save(commit=False)
                 task.done = 'doing'
+                task.user = request.user
                 task.save()
                 return redirect('/')
      else:
         form = TaskForm()
         return render(request, 'tasks/addtask.html', {'form': form})
 
+@login_required
 def editTask(request, id):
     task = get_object_or_404(Task, pk=id)
     form = TaskForm(instance=task)
@@ -55,6 +61,7 @@ def editTask(request, id):
     else:
       return render(request, 'tasks/edittask.html', {'form':form,'task':task})
 
+@login_required
 def deleteTask(request, id):
     task = get_object_or_404(Task, pk=id)
     task.delete()
--- toDoproject/settings.py
+++ toDoproject/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'tasks',
     'users',
     'crispy_forms',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -58,7 +59,7 @@ ROOT_URLCONF = 'toDoproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +124,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = '/'
+
+LOGOUT_REDIRECT_URL = '/accounts/login'
